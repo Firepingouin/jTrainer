c55c1832742aaedf1603bb6e6b9a1cc664630914
mlazzje <mlazzje@gmail.com>
2015-01-07 09:41:15 +0100

    adding main servlets

--- src/jtrain/JTrainerServlet.java
+++ src/jtrain/JTrainerServlet.java
@@ -1,13 +1,19 @@
 package jtrain;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class JTrainerServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("application/json");
+		// Get the printwriter object from resp to write the required json object to the output stream      
+		PrintWriter out = resp.getWriter();
+		String jsonObject = "{ firstname: 'Nico', lastname: 'Pari' }";
+		out.print(jsonObject);
+		out.flush();
 	}
 }
