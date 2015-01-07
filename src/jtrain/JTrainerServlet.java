9d5458ab0e467789dda11c27683c41ae11cb355c
mlazzje <mlazzje@gmail.com>
2015-01-07 09:10:01 +0100

    GAE Project

---
+++ src/jtrain/JTrainerServlet.java
@@ -0,0 +1,13 @@
+package jtrain;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class JTrainerServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
