f8f157ed3ff705948a5d0d5a5ecc57dece1ccf84
Nicolas Parimeros <nicolas.parimeros@gmail.com>
2015-01-08 17:12:41 +0100

    Search fonctionnel

--- src/jtrain/JSearchServlet.java
+++ src/jtrain/JSearchServlet.java
@@ -14,6 +14,10 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.labs.repackaged.org.json.JSONArray;
 import com.google.appengine.labs.repackaged.org.json.JSONException;
 import com.google.appengine.labs.repackaged.org.json.JSONObject;
@@ -41,7 +45,7 @@ public class JSearchServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		String requestType = req.getParameter("type");
-		
+
 		if (requestType != null) {
 			List<JSONObject> domaines = new ArrayList<JSONObject>();
 			if (requestType.equals("domaine")) {
@@ -60,12 +64,233 @@ public class JSearchServlet extends HttpServlet {
 					}
 					domaines.add(domaine);
 				}
+				resp.setContentType("application/json");
+				PrintWriter out = resp.getWriter();
+				JSONArray jsonObject = new JSONArray(domaines);
+				out.print(jsonObject.toString());
+				out.flush();
+			} else if (requestType.equals("search")) {
+				String domaineId = req.getParameter("domainId");
+				String searchKeyword = req.getParameter("searchKeyword");
+				
+				JSONObject results = new JSONObject();
+				if(domaineId != null) {
+					results = this.getAllForDomaine(domaineId);
+				} else if(searchKeyword != null) {
+					results = this.search(searchKeyword);
+				}
+				// On retourne un objet JSON contenant les résultats de la recherche
+				resp.setContentType("application/json");
+				PrintWriter out = resp.getWriter();
+				out.print(results.toString());
+				out.flush();
 			}
-			resp.setContentType("application/json");      
-			PrintWriter out = resp.getWriter();
-			JSONArray jsonObject = new JSONArray(domaines);
-			out.print(jsonObject.toString());
-			out.flush();
 		}
+	}
+
+	private JSONObject search(String searchKeyword) {
+		
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		List<JSONObject> trainingPlans = new ArrayList<JSONObject>();
+		List<JSONObject> exercices = new ArrayList<JSONObject>();
+		JSONObject results = new JSONObject();
+
+		// On filtre sur le domaine
+		Filter titreFilter = new FilterPredicate("titre",
+				FilterOperator.EQUAL, searchKeyword.toString());
+
+		// Requete sur les training plans, puis les exercices
+		Query q = new Query("TrainingPlan").setFilter(titreFilter);
+		PreparedQuery pq = datastore.prepare(q);
+
+		// Pour chaque training plan correspondant
+		for (Entity trainingPlan : pq.asIterable()) {
+			// Récupération des attributs du training plan
+			String titre = (String) trainingPlan.getProperty("titre");
+			String description = (String) trainingPlan
+					.getProperty("description");
+			Long trainingPlanId = trainingPlan.getKey().getId();
+			Long domaineId = Long.parseLong(trainingPlan.getProperty("domaineId").toString());
+
+			try {
+				// On construit l'objet JSON du training plan
+				JSONObject trainingPlanJson = new JSONObject();
+
+				// On set les attributs de l'objet
+				trainingPlanJson.put("titre", titre);
+				trainingPlanJson.put("description", description);
+				trainingPlanJson.put("domaineId", domaineId);
+				int dureeTotaleTrainingPlan = 0;
+
+				// On requête les exercices pour obtenir la durée totale du
+				// training plan
+				Filter exerciceFilter = new FilterPredicate("trainingPlanId",
+						FilterOperator.EQUAL, trainingPlanId);
+				Query totalTimeQuery = new Query("Exercice")
+						.setFilter(exerciceFilter);
+				PreparedQuery ptotalTimeQuery = datastore
+						.prepare(totalTimeQuery);
+
+				// Pour chaque exercice trouvé on somme la durée à la durée totale
+				for (Entity exercice : ptotalTimeQuery.asIterable()) {
+					// On somme la durée
+					long duree = (long) exercice.getProperty("duree");
+					dureeTotaleTrainingPlan += duree;
+				}
+
+				// On ajoute la durée totale au training plan
+				trainingPlanJson.put("duree", dureeTotaleTrainingPlan);
+
+				// On ajoute le training plan au tableau de résultats
+				trainingPlans.add(trainingPlanJson);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// On requête sur les exercices
+		q = new Query("Exercice").setFilter(titreFilter);
+		pq = datastore.prepare(q);
+		
+		// Pour chaque exercice correspondant
+		for (Entity exercice : pq.asIterable()) {
+			// Récupération des attributs du training plan
+			String titre = (String) exercice.getProperty("titre");
+			String description = (String) exercice.getProperty("description");
+			int duree = Integer.parseInt(exercice.getProperty("duree").toString());
+			int repetitions = Integer.parseInt(exercice.getProperty("repetitions").toString());
+			Long trainingPlanId = Long.parseLong(exercice.getProperty("trainingPlanId").toString());
+			Long exerciceId = exercice.getKey().getId();
+
+			try {
+				// On construit l'objet JSON de l'exercice
+				JSONObject exerciceJson = new JSONObject();
+
+				// On set les attributs de l'objet
+				exerciceJson.put("titre", titre);
+				exerciceJson.put("description", description);
+				exerciceJson.put("duree", duree);
+				exerciceJson.put("repetitions", repetitions);
+				exerciceJson.put("trainingPlanId", trainingPlanId);
+				exerciceJson.put("exerciceId", exerciceId);
+				
+				// On ajoute l'exercice au tableau de résultats
+				exercices.add(exerciceJson);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// On construit des Array JSON a partir des Array construits
+		JSONArray trainingPlansArray = new JSONArray(trainingPlans);
+		JSONArray exercicesArray = new JSONArray(exercices);
+
+		// On insère les JSONArray dans l'objet résultat JSON
+		try {
+			results.put("trainingPlans", trainingPlansArray);
+			results.put("exercices", exercicesArray);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		// On retourne les résultats trouvés en Objet Json
+		return results;
+	}
+
+	private JSONObject getAllForDomaine(String domaineId) {
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		List<JSONObject> trainingPlans = new ArrayList<JSONObject>();
+		List<JSONObject> exercices = new ArrayList<JSONObject>();
+		JSONObject results = new JSONObject();
+
+		// On filtre sur le domaine
+		Filter trainingPlanFilter = new FilterPredicate("domaineId",
+				FilterOperator.EQUAL, Long.parseLong(domaineId.toString()));
+
+		// Requete sur les training plans, puis les exercices
+		Query q = new Query("TrainingPlan").setFilter(trainingPlanFilter);
+		PreparedQuery pq = datastore.prepare(q);
+
+		// Pour chaque training plan correspondant
+		for (Entity trainingPlan : pq.asIterable()) {
+			// Récupération des attributs du training plan
+			String titre = (String) trainingPlan.getProperty("titre");
+			String description = (String) trainingPlan
+					.getProperty("description");
+			Long trainingPlanId = trainingPlan.getKey().getId();
+
+			try {
+				// On construit l'objet JSON du training plan
+				JSONObject trainingPlanJson = new JSONObject();
+
+				// On set les attributs de l'objet
+				trainingPlanJson.put("titre", titre);
+				trainingPlanJson.put("description", description);
+				trainingPlanJson.put("domaineId", domaineId);
+				int dureeTotaleTrainingPlan = 0;
+
+				// On requête les exercices pour obtenir la durée totale du
+				// training plan
+				Filter exerciceFilter = new FilterPredicate("trainingPlanId",
+						FilterOperator.EQUAL, trainingPlanId);
+				Query totalTimeQuery = new Query("Exercice")
+						.setFilter(exerciceFilter);
+				PreparedQuery ptotalTimeQuery = datastore
+						.prepare(totalTimeQuery);
+
+				// Pour chaque exercice trouvé on somme la durée à la durée
+				// totale, et on construit un objet JSON
+				for (Entity exercice : ptotalTimeQuery.asIterable()) {
+
+					// On somme la durée
+					long duree = (long) exercice.getProperty("duree");
+					dureeTotaleTrainingPlan += duree;
+
+					// On récupère les attributs de l'exercice
+					String titreExercice = (String) exercice
+							.getProperty("titre");
+					String descriptionExercice = (String) trainingPlan
+							.getProperty("description");
+					long repetitions = (long) exercice
+							.getProperty("repetitions");
+
+					// On construit l'objet JSON de l'exercice
+					JSONObject exerciceJson = new JSONObject();
+					exerciceJson.put("titre", titreExercice);
+					exerciceJson.put("description", descriptionExercice);
+					exerciceJson.put("duree", duree);
+					exerciceJson.put("repetitions", repetitions);
+					exerciceJson.put("trainingPlanId", trainingPlanId);
+
+					// On ajoute l'exercice au tableau de résultats
+					exercices.add(exerciceJson);
+				}
+
+				// On ajoute la durée totale au training plan
+				trainingPlanJson.put("duree", dureeTotaleTrainingPlan);
+
+				// On ajoute le training plan au tableau de résultats
+				trainingPlans.add(trainingPlanJson);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// On construit des Array JSON a partir des Array construits
+		JSONArray trainingPlansArray = new JSONArray(trainingPlans);
+		JSONArray exercicesArray = new JSONArray(exercices);
+
+		// On insère les JSONArray dans l'objet résultat JSON
+		try {
+			results.put("trainingPlans", trainingPlansArray);
+			results.put("exercices", exercicesArray);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		// On retourne les résultats trouvés en Objet Json
+		return results;
 	}
 }
